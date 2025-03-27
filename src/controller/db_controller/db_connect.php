06a058d14b563e732b1844dc5e3ae30988059d2b
dabeo <ngob5405@gmail.com>
2025-03-27 08:09:12 +0700

    1

--- src/controller/db_controller/db_connect.php
+++ src/controller/db_controller/db_connect.php
@@ -5,9 +5,10 @@
         $dbname = 'bangiay_db'; 
         $username = 'root'; 
         $password = ''; 
+        $port = 3307;
 
         try {
-            $pdo = new PDO("mysql:host=$host;port=3307;dbname=$dbname;charset=utf8", $username, $password);
+            $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // echo 'kết nối thành công';
         } catch (PDOException $e) {
