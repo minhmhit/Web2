48eac3c0f185bae177c91b5ece1f28ba52dce463
ThayMing <minh.maihoang.71@gmail.com>
2025-04-17 07:41:02 +0700

    backup before test

--- src/admin/views/suppliers/detail.php
+++ src/admin/views/suppliers/detail.php
@@ -3,32 +3,7 @@
 
 <head>
     <title>Chi tiết nhà cung cấp</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
 
-        th,
-        td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-
-        a {
-            text-decoration: none;
-            color: blue;
-        }
-
-        a:hover {
-            text-decoration: underline;
-        }
-    </style>
 </head>
 
 <body>
@@ -70,5 +45,28 @@
     <br>
     <a href="admin.php?page=suppliers&action=list">Quay lại danh sách</a>
 </body>
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    th,
+    td {
+        border: 1px solid black;
+        padding: 8px;
+        text-align: left;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+
+   
+
+    a:hover {
+        text-decoration: underline;
+    }
+</style>
 
 </html>
